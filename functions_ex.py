b2c7c3fe6284ba880dfbb584a1b8fe247e08c2ff
CSoft56 <careersoftsolutions56@gmail.co>
2023-03-10 18:30:11 -0500

    Commiting functions ex..

---
+++ functions_ex.py
@@ -0,0 +1,21 @@
+"""
+Create a function to find sum of given two arguments
+
+1. function definition
+2. calling function with function name
+
+
+1. Write a function to find max value in given list
+2. Write a function to remove duplicates from a given list
+3. Write a function to combine given two dictionaries and return
+4. Write a function to print all odd numbers in given list
+5. Write a function to print all prime numbers in given list
+
+"""
+
+def sum_args(a: int, b: str):
+    s = 2 * a
+    return s
+
+res = sum_args(10)
+print(res)
